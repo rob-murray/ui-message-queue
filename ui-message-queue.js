63be3900f6b7823a8522ada192fe4e040df79c41
Rob Murray <robmurray17@gmail.com>
2012-08-01 19:27:47 +0100

    Created array in array literal notation

--- ui-message-queue.js
+++ ui-message-queue.js
@@ -1,20 +1,19 @@
-Copyright (C) 2012 Rob Murray
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
+/*
+ui-message-queue - a User Message Queue Javascript implementation
+(c) 2012 Robert Murray
+@see https://github.com/rob-murray/ui-message-queue
+UiMessageQueue may be freely distributed under the MIT license
+*/
 
 (function() {
   var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
   window.UiMessageQueue = (function() {
-
+    "use strict";
     UiMessageQueue.prototype.messageBoxDivEl = null;
 
-    UiMessageQueue.prototype.messagesArr = new Array();
+    UiMessageQueue.prototype.messagesArr = [];
 
     UiMessageQueue.prototype.messagesCount = 0;
 
@@ -104,4 +103,3 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
   })();
 
 }).call(this);
-
